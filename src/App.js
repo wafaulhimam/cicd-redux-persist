a5a076cbeaacf815f2eb090062f72ee568a51e8b
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2021-02-16 06:55:57 +0700

    add pipeline gitlab

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Ubah Dikid
         </a>
       </header>
     </div>
