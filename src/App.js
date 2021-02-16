5bce9d2b4a430cdff0af19d5eaa1f2e0de9b4f66
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2021-02-16 14:09:23 +0700

    Edit appjs to test pipeline deployment cicd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ubah Dikid
+          Halo ubah sedikit dari ini untuk test CICD
         </a>
       </header>
     </div>
